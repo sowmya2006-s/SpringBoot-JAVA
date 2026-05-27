5c66eefdf65fa071c067fc045a346a3b68e34250
Sowmya <sowmyasakthivelcbe@gmail.com>
2026-05-27 14:02:16 +0530

    Dependency Injection

---
+++ myApp/src/main/java/com/telusko/myApp/Dev.java
@@ -0,0 +1,11 @@
+package com.telusko.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("Sowwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+    }
+}
