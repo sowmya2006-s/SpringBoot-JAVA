176f3bc0cbf9e4568aac1d9b48e0a9ea620b04be
Sowmya <sowmyasakthivelcbe@gmail.com>
2026-05-29 12:05:55 +0530

    fixing error

--- myApp/src/main/java/com/telusko/myApp/Dev.java
+++ myApp/src/main/java/com/telusko/myApp/Dev.java
@@ -1,12 +1,12 @@
 package com.telusko.myApp;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
     @Autowired
-    @Qualifier("Desktop")
+    @Qualifier("desktop")
     Computer c;
 
     public void build() {
